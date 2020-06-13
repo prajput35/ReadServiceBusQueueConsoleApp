10eacf7d5048d7f841501d209126e3a529196469
Paras Rajput <paras.code.api@gmail.com>
2020-06-13 19:57:16 +0530

    Add project files.

---
+++ queue-receive/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace queue_receive
+{
+    class Program
+    {
+        private static string _bus_connectionstring = "Endpoint=sb://demoservicebusns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fVsy3ecOuLowdfn84d9Pr15A539zqepvO5VqGbkUH7I=";
+        private static string _queue_name = "demoqueue";
+        private static QueueClient _client;
+
+        static void Main(string[] args)
+        {
+            QueueFunction().GetAwaiter().GetResult();
+        }
+
+        static async Task QueueFunction()
+        {
+                _client = new QueueClient(_bus_connectionstring, _queue_name);
+
+                var _options = new MessageHandlerOptions(ExceptionReceived)
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                };
+
+                _client.RegisterMessageHandler(Process_Message, _options);
+            Console.ReadKey();
+            }
+        
+
+        static async Task Process_Message(Message _message,CancellationToken _token)
+        {
+            Console.WriteLine($"Message ID : {Encoding.UTF8.GetString(_message.Body)}");
+            
+
+            await _client.CompleteAsync(_message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceived(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+
+    }
+}
